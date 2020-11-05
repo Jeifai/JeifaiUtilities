14aaffc49bea5b104c06397aefd2b29ab12fa450
Roberto Malcotti <roberto.malcotti@datangels.com>
2020-11-05 18:33:41 +0100

    first dirty working prototype

--- ML_prototype/create_dataset.py
+++ ML_prototype/create_dataset.py
@@ -6,12 +6,20 @@ df_location = pd.read_csv('places_dataset/locations_with_places_and_countries.cs
 
 df_join = pd.merge(df_main, df_location, on ='location', how ='left')
 
-df_join = df_join[['company', 'day', 'month', 'days', 'is_it_job', 'seniority', 'country']]
-
 df_join.dropna(inplace=True)
 
 df_join = df_join[df_join['days'] > 0]
 
+df_join.company = pd.Categorical(df_join.company)
+df_join['company_code'] = df_join.company.cat.codes
+
+df_join.country = pd.Categorical(df_join.country)
+df_join['country_code'] = df_join.country.cat.codes
+
+df_join = df_join[['day', 'month', 'days', 'is_it_job', 'seniority', 'company_code', 'country_code']]
+
 print(df_join.head(5))
+
+df_join.to_csv('ml_dataset.csv')
 
 # print(pd.pivot_table(df_join, values='company', index=['days'], aggfunc='count'))
---
+++ ML_prototype/test_ml.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+# Loading and normalizing
+import torch.utils.data as data_utils
+dataset = pd.read_csv('ml_dataset.csv')
+
+categorical_columns = ['seniority', 'company_code', 'country_code']
+numerical_columns = ['day', 'month', 'days']
+outputs = ['is_it_job']
+
+for category in categorical_columns:
+    dataset[category] = dataset[category].astype('category')
+
+seniority = dataset['seniority'].cat.codes.values
+company_code = dataset['company_code'].cat.codes.values
+country_code = dataset['country_code'].cat.codes.values
+
+categorical_data = np.stack([seniority, company_code, country_code], 1)
+
+categorical_data = torch.tensor(categorical_data, dtype=torch.int64)
+
+numerical_data = np.stack([dataset[col].values for col in numerical_columns], 1)
+numerical_data = torch.tensor(numerical_data, dtype=torch.float)
+
+outputs = torch.tensor(dataset[outputs].values).flatten()
+
+categorical_column_sizes = [len(dataset[column].cat.categories) for column in categorical_columns]
+categorical_embedding_sizes = [(col_size, min(50, (col_size+1)//2)) for col_size in categorical_column_sizes]
+
+
+total_records = len(dataset)
+test_records = int(total_records * .2)
+
+categorical_train_data = categorical_data[:total_records-test_records]
+categorical_test_data = categorical_data[total_records-test_records:total_records]
+numerical_train_data = numerical_data[:total_records-test_records]
+numerical_test_data = numerical_data[total_records-test_records:total_records]
+train_outputs = outputs[:total_records-test_records]
+test_outputs = outputs[total_records-test_records:total_records]
+
+class Model(nn.Module):
+
+    def __init__(self, embedding_size, num_numerical_cols, output_size, layers, p=0.4):
+        super().__init__()
+        self.all_embeddings = nn.ModuleList([nn.Embedding(ni, nf) for ni, nf in embedding_size])
+        self.embedding_dropout = nn.Dropout(p)
+        self.batch_norm_num = nn.BatchNorm1d(num_numerical_cols)
+
+        all_layers = []
+        num_categorical_cols = sum((nf for ni, nf in embedding_size))
+        input_size = num_categorical_cols + num_numerical_cols
+
+        for i in layers:
+            all_layers.append(nn.Linear(input_size, i))
+            all_layers.append(nn.ReLU(inplace=True))
+            all_layers.append(nn.BatchNorm1d(i))
+            all_layers.append(nn.Dropout(p))
+            input_size = i
+
+        all_layers.append(nn.Linear(layers[-1], output_size))
+
+        self.layers = nn.Sequential(*all_layers)
+
+    def forward(self, x_categorical, x_numerical):
+        embeddings = []
+        for i,e in enumerate(self.all_embeddings):
+            embeddings.append(e(x_categorical[:,i]))
+        x = torch.cat(embeddings, 1)
+        x = self.embedding_dropout(x)
+
+        x_numerical = self.batch_norm_num(x_numerical)
+        x = torch.cat([x, x_numerical], 1)
+        x = self.layers(x)
+        return x
+
+model = Model(categorical_embedding_sizes, numerical_data.shape[1], 2, [200,100,50], p=0.4)
+# print(model)
+
+loss_function = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+
+epochs = 10
+aggregated_losses = []
+
+for i in range(epochs):
+    i += 1
+    y_pred = model(categorical_train_data, numerical_train_data)
+    single_loss = loss_function(y_pred, train_outputs)
+    aggregated_losses.append(single_loss)
+
+    if i%25 == 1:
+        print("epoch: " + str(i) + "\tloss: " + str(single_loss.item()))
+
+    optimizer.zero_grad()
+    single_loss.backward()
+    optimizer.step()
+
+print("epoch: " + str(i) + "\tloss: " + str(single_loss.item()))
+
+plt.plot(range(epochs), aggregated_losses)
+plt.ylabel('Loss')
+plt.xlabel('epoch');
+
+with torch.no_grad():
+    y_val = model(categorical_test_data, numerical_test_data)
+    loss = loss_function(y_val, test_outputs)
+print("Loss: " + str(loss))
